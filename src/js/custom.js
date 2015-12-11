cd31c3864412fe95797cfb6f275daeec2259f90c
unknown <litor.rostov.ru@gmail.com>
2015-12-11 16:16:47 +0300

    srcond commit

---
+++ src/js/custom.js
@@ -0,0 +1,16 @@
+/**************************************/
+/* Custom JavaScript files supervisor */
+/**************************************/
+
+$(document).ready(function() {
+
+    /* Custom */
+ /*    //= ./common/material-init.js */
+ /*    //= ./common/google-analytics.js */
+  $(".mbr").height(window.innerHeight);
+  //$(".mbr-main-timur__block-fluid").width(window.innerWidth);
+  $(".mbr-main-timur__block-fluid").css("width": window.innerWidth + "px");
+
+
+  
+});
